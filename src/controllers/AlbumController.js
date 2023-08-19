8240c1e233e6a31debd42f2f89a97873b1922893
Filipe Bacof <filipebacof@gmail.com>
2023-08-19 13:10:28 -0300

    feat: atualizar compositores com album criado

--- src/controllers/AlbumController.js
+++ src/controllers/AlbumController.js
@@ -1,4 +1,5 @@
 const AlbumRepository = require('../repositories/AlbumRepository')
+const BandMemberRepository = require('../repositories/BandMemberRepository')
 
 class AlbumController {
   async index(request, response) {
@@ -59,7 +60,26 @@ class AlbumController {
         composers,
       })
 
-      album.save()
+      await album.save()
+      const composerPromises = composers.map(async (idComposer) => {
+        try {
+          const bandMember = await BandMemberRepository.findById(idComposer)
+          if (!bandMember) {
+            console.log(`Band member with ID ${idComposer} not found.`)
+            return null
+          }
+          const albunsOnMemberUpdated = [...bandMember.albuns, album._id]
+          await BandMemberRepository.findByIdAndUpdate(idComposer, {
+            albuns: albunsOnMemberUpdated,
+          })
+          return idComposer
+        } catch (error) {
+          console.log(`Error updating band member ${idComposer}:`, error)
+          return null
+        }
+      })
+
+      await Promise.all(composerPromises)
 
       return response.status(200).json(album)
     } catch (error) {
@@ -73,14 +93,16 @@ class AlbumController {
       request.body
 
     try {
-      await AlbumRepository.findByIdAndUpdate(id, {
+      const updatedFields = {
         ...(title && { title }),
         ...(albumCover && { albumCover }),
         ...(tracks && { tracks }),
         ...(releaseDate && { releaseDate }),
         ...(spotifyURL && { spotifyURL }),
         ...(composers && { composers }),
-      })
+      }
+
+      await AlbumRepository.findByIdAndUpdate(id, updatedFields)
 
       const updatedAlbum = await AlbumRepository.findById(id)
 
@@ -90,6 +112,7 @@ class AlbumController {
       })
     } catch (error) {
       console.log(error)
+      return response.status(500).json({ message: 'Erro interno do servidor' })
     }
   }
 
@@ -99,10 +122,27 @@ class AlbumController {
     try {
       const album = await AlbumRepository.findById(id)
 
-      if (!album)
-        return response
-          .status(404)
-          .json({ message: 'Este album não foi encontrado.' })
+      if (!album) {
+        return response.status(404).json({ message: 'Album não encontrado.' })
+      }
+
+      // const composerPromises = album.composers.map(async (idComposer) => {
+      //   try {
+      //     const bandMember = await BandMemberRepository.findById(idComposer)
+      //     if (bandMember) {
+      //       bandMember.albuns = bandMember.albuns.filter(
+      //         (albumId) => albumId.toString() !== id,
+      //       )
+      //       await bandMember.save()
+      //     }
+      //     return idComposer
+      //   } catch (error) {
+      //     console.log(`Error updating band member ${idComposer}:`, error)
+      //     return null
+      //   }
+      // })
+
+      // await Promise.all(composerPromises)
 
       await AlbumRepository.delete(id)
 
@@ -111,6 +151,7 @@ class AlbumController {
         .json({ message: 'Album deletado com sucesso.' })
     } catch (error) {
       console.log(error)
+      return response.status(500).json({ message: 'Erro interno do servidor' })
     }
   }
 }
